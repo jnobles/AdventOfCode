22fe471f20916917cfee213e43e2333b27953873
inforussle <123Jimmyn@gmail.com>
2022-12-06 09:52:42 -0500

    Day 6 solved

---
+++ 2022/day_6/day_6-part_2.py
@@ -0,0 +1,16 @@
+def locate_marker(packet: str) -> int:
+    active = packet[0:14]
+    characters_processed = 14
+
+    valid_marker = all([active.count(x) == 1 for x in active])
+    while valid_marker == False:
+        characters_processed += 1
+        active = packet[characters_processed-14:characters_processed]
+        valid_marker = all([active.count(x) == 1 for x in active])
+
+    return characters_processed
+
+with open('day_6-input') as f:
+    packet = f.readline().rstrip()
+
+print(locate_marker(packet))
