4a42c286b3dee4a2619b4dc151d704725e1541cb
James N <123Jimmyn@gmail.com>
2024-12-07 19:10:49 -0500

    If the brute force is in base 3, is it still brute force?

---
+++ 2024/day07/part_2.py
@@ -0,0 +1,49 @@
+with open("puzzle_input") as f:
+    puzzle_input = [line.strip() for line in f.readlines()]
+
+calibration_list = []
+for line in puzzle_input:
+    product, inputs = line.split(": ")
+    product = int(product)
+    inputs = list(int(val) for val in inputs.split(" "))
+    calibration_list.append([product, inputs])
+
+
+def get_next_operator_combination(item_count, previous_attempt):
+    if previous_attempt == "".join(["|" for _ in range(item_count - 1)]):
+        return False
+    bin_value = int(previous_attempt.replace("+", "0").replace("*", "1").replace("|", "2"), base=3)
+    bin_value += 1
+    next_combination = get_terniary_string_representation(bin_value).replace("0", "+").replace("1", "*").replace("2", "|")
+    while len(next_combination) < item_count - 1:
+        next_combination = "+" + next_combination
+    return next_combination
+
+
+def get_terniary_string_representation(value):
+    result = ""
+    while value != 0:
+        value, remainder = divmod(value, 3)
+        result = str(remainder) + result
+    return result
+
+
+calibration_result = 0
+for test in calibration_list:
+    product, inputs = test
+    attempt = "".join(["+" for _ in range(len(inputs) - 1)])
+    while attempt:
+        value = inputs[0]
+        for i, operator in enumerate(attempt):
+            if operator == "+":
+                value += inputs[i+1]
+            elif operator == "*":
+                value *= inputs[i+1]
+            elif operator == "|":
+                value = int(str(value) + str(inputs[i+1]))
+        if value == product:
+            print(f"{product} is valid: {attempt}")
+            calibration_result += product
+            break
+        attempt = get_next_operator_combination(len(inputs), attempt)
+print(calibration_result)
